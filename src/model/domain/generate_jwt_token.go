ceab07edfe4cec5d0f519c4f373387d7cba034f8
william-tome <william.tome@azion.com>
2023-10-21 18:44:50 -0300

    feat: implement logger in token generation method

--- src/model/domain/generate_jwt_token.go
+++ src/model/domain/generate_jwt_token.go
@@ -6,9 +6,12 @@ import (
 
 	"github.com/golang-jwt/jwt"
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 )
 
 func (u *tUser) GenerateToken() (string, *ierrors.TError) {
+	logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["INIT"]["TOKEN_GENERATION"])
+
 	secret := os.Getenv("JWT_SEED")
 
 	claims := jwt.MapClaims{
@@ -20,8 +23,11 @@ func (u *tUser) GenerateToken() (string, *ierrors.TError) {
 	tokenString, err := token.SignedString([]byte(secret))
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["AUTH"], logger.MESSAGE["ERROR"]["TOKEN_GENERATION"])
 		return "", ierrors.NewInternalError()
 	}
+
+	logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["OK"]["TOKEN_GENERATION"])
 
 	return tokenString, nil
 }
