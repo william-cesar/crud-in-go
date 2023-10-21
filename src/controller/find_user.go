a60c7217ec6cf260fcc780fd4123652acd4df2d7
william-tome <william.tome@azion.com>
2023-10-21 18:25:07 -0300

    feat: implement logger in find user controller

--- src/controller/find_user.go
+++ src/controller/find_user.go
@@ -8,14 +8,18 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"github.com/william-cesar/crud-in-go/src/view/adapters"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (uc *tUserController) FindUserByEmail(c *gin.Context) {
+	logger.NewInfoLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["INIT"]["FIND_EMAIL"])
+
 	body, err := io.ReadAll(c.Request.Body)
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["ERROR"]["REQ_BODY"])
 		e := ierrors.NewBadRequestError()
 		c.JSON(e.StatusCode, e)
 		return
@@ -24,6 +28,7 @@ func (uc *tUserController) FindUserByEmail(c *gin.Context) {
 	var u adapters.TUserEmailRequest
 
 	if err = json.Unmarshal(body, &u); err != nil {
+		logger.NewErrorLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["ERROR"]["UNMARSHAL"])
 		e := ierrors.NewInternalError()
 		c.JSON(e.StatusCode, e)
 		return
@@ -32,17 +37,22 @@ func (uc *tUserController) FindUserByEmail(c *gin.Context) {
 	user, uErr := uc.service.FindUserByEmailService(u.Email)
 
 	if uErr != nil {
+		logger.NewErrorLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["ERROR"]["NO_USER"])
 		c.JSON(uErr.StatusCode, uErr)
 		return
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["OK"]["FOUND"])
 	c.JSON(http.StatusOK, adapters.ConvertDomainToResponse(user))
 }
 
 func (uc *tUserController) FindUserById(c *gin.Context) {
+	logger.NewInfoLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["INIT"]["FIND_ID"])
+
 	id := c.Param("id")
 
 	if _, err := primitive.ObjectIDFromHex(id); err != nil || strings.TrimSpace(id) == "" {
+		logger.NewErrorLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["ERROR"]["INVALID_ID"])
 		e := ierrors.NewBadRequestError()
 		c.JSON(e.StatusCode, e)
 		return
@@ -51,9 +61,11 @@ func (uc *tUserController) FindUserById(c *gin.Context) {
 	user, uErr := uc.service.FindUserByIdService(id)
 
 	if uErr != nil {
+		logger.NewErrorLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["ERROR"]["NO_USER"])
 		c.JSON(uErr.StatusCode, uErr)
 		return
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["FIND_CONTROLLER"], logger.MESSAGE["OK"]["FOUND"])
 	c.JSON(http.StatusOK, adapters.ConvertDomainToResponse(user))
 }
