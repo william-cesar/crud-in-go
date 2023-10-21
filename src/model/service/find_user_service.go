8d289ce66582acac1859e86c48985ae08c888873
william-tome <william.tome@azion.com>
2023-10-21 19:23:13 -0300

    feat: implement logger in find user service

--- src/model/service/find_user_service.go
+++ src/model/service/find_user_service.go
@@ -2,31 +2,47 @@ package service
 
 import (
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"github.com/william-cesar/crud-in-go/src/model/domain"
 )
 
 func (us *tUserService) FindUserByEmailService(email string) (domain.IUser, *ierrors.TError) {
+	logger.NewInfoLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["INIT"]["FIND_EMAIL"])
+
 	user, err := us.repository.FindUserByEmail(email)
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["ERROR"]["NO_USER"])
 		return nil, err
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["OK"]["FOUND"])
 	return user, nil
 }
 
 func (us *tUserService) FindUserByIdService(id string) (domain.IUser, *ierrors.TError) {
+	logger.NewInfoLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["INIT"]["FIND_ID"])
+
 	user, err := us.repository.FindUserById(id)
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["ERROR"]["NO_USER"])
 		return nil, err
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["OK"]["FOUND"])
 	return user, nil
 }
 
 func (us *tUserService) findUserByEmailAndPassword(credentials domain.IUser) *ierrors.TError {
+	logger.NewInfoLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["INIT"]["FIND_EMAIL_PASS"])
+
 	err := us.repository.FindUserByEmailAndPassword(credentials)
 
+	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["ERROR"]["NO_USER"])
+	}
+
+	logger.NewInfoLog(logger.JOURNEY["FIND_SERVICE"], logger.MESSAGE["OK"]["FOUND"])
 	return err
 }
