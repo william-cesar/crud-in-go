c0ab81b4aa82c807084b596cbf8e81a87fed1e9e
william-tome <william.tome@azion.com>
2023-10-21 18:58:52 -0300

    feat: implement logger in create user repository

--- src/model/repository/create_user_repository.go
+++ src/model/repository/create_user_repository.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"github.com/william-cesar/crud-in-go/src/model/adapters"
 	"github.com/william-cesar/crud-in-go/src/model/domain"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (ur *tUserRepository) CreateUser(d domain.IUser) (domain.IUser, *ierrors.TError) {
+	logger.NewInfoLog(logger.JOURNEY["CREATE_REPOSITORY"], logger.MESSAGE["INIT"]["CREATION"])
+
 	collection := ur.dbconn.Collection(COLLECTION)
 
 	dbEntity := adapters.ConvertDomainToEntity(d)
@@ -17,10 +20,12 @@ func (ur *tUserRepository) CreateUser(d domain.IUser) (domain.IUser, *ierrors.TE
 	res, err := collection.InsertOne(context.Background(), dbEntity)
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["CREATE_REPOSITORY"], logger.MESSAGE["ERROR"]["CREATION"])
 		return nil, ierrors.NewInternalError(err.Error())
 	}
 
 	dbEntity.ID = res.InsertedID.(primitive.ObjectID)
 
+	logger.NewInfoLog(logger.JOURNEY["CREATE_REPOSITORY"], logger.MESSAGE["OK"]["CREATED"])
 	return adapters.ConvertEntityToDomain(*dbEntity), nil
 }
