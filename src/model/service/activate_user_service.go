0340040a44be8f5f09a050d7783aeaca92a08cbe
william-tome <william.tome@azion.com>
2023-10-21 19:04:03 -0300

    feat: implement logger in active user repository

--- src/model/service/activate_user_service.go
+++ src/model/service/activate_user_service.go
@@ -2,10 +2,18 @@ package service
 
 import (
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 )
 
 func (us *tUserService) ActivateUserService(id string) *ierrors.TError {
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE_SERVICE"], logger.MESSAGE["INIT"]["ACTIVATION"])
+
 	err := us.repository.ActivateUser(id)
 
+	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["ACTIVATE_SERVICE"], logger.MESSAGE["ERROR"]["ACTIVATION"])
+	}
+
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE_SERVICE"], logger.MESSAGE["OK"]["ACTIVE"])
 	return err
 }
