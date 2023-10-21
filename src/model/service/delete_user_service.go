d17d3d180ac8fd19f37ffa843b984a3412f2934b
william-tome <william.tome@azion.com>
2023-10-21 19:17:40 -0300

    feat: implement logger in delete user service

--- src/model/service/delete_user_service.go
+++ src/model/service/delete_user_service.go
@@ -2,10 +2,18 @@ package service
 
 import (
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 )
 
 func (us *tUserService) DeleteUserService(id string) *ierrors.TError {
+	logger.NewInfoLog(logger.JOURNEY["DELETE_SERVICE"], logger.MESSAGE["INIT"]["DELETION"])
+
 	err := us.repository.DeleteUser(id)
 
+	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["DELETE_SERVICE"], logger.MESSAGE["ERROR"]["DELETION"])
+	}
+
+	logger.NewInfoLog(logger.JOURNEY["DELETE_SERVICE"], logger.MESSAGE["OK"]["DELETED"])
 	return err
 }
