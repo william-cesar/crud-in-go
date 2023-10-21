eb137ad50682ff1232000bc08cfbae21bbb89ec5
william-tome <william.tome@azion.com>
2023-10-20 23:18:36 -0300

    feat: create findById in repository and add active verification

--- src/model/repository/find_user_repository.go
+++ src/model/repository/find_user_repository.go
@@ -8,10 +8,11 @@ import (
 	d "github.com/william-cesar/crud-in-go/src/model/domain"
 	"github.com/william-cesar/crud-in-go/src/model/entity"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (ur *tUserRepository) FindUser(email string) (d.IUser, *ierrors.TError) {
+func (ur *tUserRepository) FindUserByEmail(email string) (d.IUser, *ierrors.TError) {
 	collection := ur.dbconn.Collection(COLLECTION)
 
 	dbEntity := &entity.TuserEntity{}
@@ -26,6 +27,33 @@ func (ur *tUserRepository) FindUser(email string) (d.IUser, *ierrors.TError) {
 		return nil, ierrors.NewInternalError()
 	}
 
-	return ma.ConvertEntityToDomain(*dbEntity), nil
+	if !dbEntity.Active {
+		return nil, ierrors.NewBadRequestError("User is not activated.")
+	}
 
+	return ma.ConvertEntityToDomain(*dbEntity), nil
+}
+
+func (ur *tUserRepository) FindUserById(id string) (d.IUser, *ierrors.TError) {
+	collection := ur.dbconn.Collection(COLLECTION)
+
+	dbEntity := &entity.TuserEntity{}
+
+	userId, _ := primitive.ObjectIDFromHex(id)
+
+	filter := bson.D{{Key: "_id", Value: userId}}
+	err := collection.FindOne(context.Background(), filter).Decode(dbEntity)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, ierrors.NewNotFoundError()
+		}
+		return nil, ierrors.NewInternalError()
+	}
+
+	if !dbEntity.Active {
+		return nil, ierrors.NewBadRequestError("User is not activated.")
+	}
+
+	return ma.ConvertEntityToDomain(*dbEntity), nil
 }
