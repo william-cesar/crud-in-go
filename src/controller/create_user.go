936c5c0ec94d391a0c6273f754cad1e51d6876da
william-tome <william.tome@azion.com>
2023-10-21 18:15:52 -0300

    feat: implement logger in create user controller

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -4,15 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"github.com/william-cesar/crud-in-go/src/config/validations"
 	"github.com/william-cesar/crud-in-go/src/model/domain"
 	"github.com/william-cesar/crud-in-go/src/view/adapters"
 )
 
 func (uc *tUserController) CreateUser(c *gin.Context) {
+	logger.NewInfoLog(logger.JOURNEY["CREATE_CONTROLLER"], logger.MESSAGE["INIT"]["CREATION"])
+
 	var userRequest adapters.TUserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.NewErrorLog(logger.JOURNEY["CREATE_CONTROLLER"], logger.MESSAGE["ERROR"]["INVALID_REQ"])
 		reqErr := validations.ValidateUserError(err)
 		c.JSON(reqErr.StatusCode, reqErr)
 		return
@@ -30,9 +34,12 @@ func (uc *tUserController) CreateUser(c *gin.Context) {
 	res, err := uc.service.CreateUserService(newUser)
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["CREATE_CONTROLLER"], logger.MESSAGE["ERROR"]["CREATION"])
 		c.JSON(err.StatusCode, err)
 		return
 	}
+
+	logger.NewInfoLog(logger.JOURNEY["CREATE_CONTROLLER"], logger.MESSAGE["OK"]["CREATED"])
 
 	c.JSON(http.StatusCreated, adapters.ConvertDomainToResponse(res))
 }
