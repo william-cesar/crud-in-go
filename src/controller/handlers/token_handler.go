b6306742255ac359a3842ef78d8e41f33c2c788f
william-tome <william.tome@azion.com>
2023-10-21 18:37:11 -0300

    feat: implement logger in token handler

--- src/controller/handlers/token_handler.go
+++ src/controller/handlers/token_handler.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/golang-jwt/jwt"
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 )
 
 func RemoveBrearePrefix(token string) string {
@@ -17,25 +18,31 @@ func RemoveBrearePrefix(token string) string {
 }
 
 func VerifyToken(tk string) *ierrors.TError {
+	logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["INIT"]["TOKEN_VERIFY"])
+
 	secret := os.Getenv("JWT_SEED")
 	trimmedToken := RemoveBrearePrefix(tk)
 
 	token, err := jwt.Parse(trimmedToken, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			logger.NewErrorLog(logger.JOURNEY["AUTH"], logger.MESSAGE["ERROR"]["TOKEN_SIGNATURE"])
 			return nil, ierrors.NewBadRequestError("Invalid token")
 		}
 		return []byte(secret), nil
 	})
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["AUTH"], logger.MESSAGE["ERROR"]["TOKEN_VERIFY"])
 		return ierrors.NewUnauthorizedError()
 	}
 
 	_, ok := token.Claims.(jwt.MapClaims)
 
 	if !ok || !token.Valid {
+		logger.NewErrorLog(logger.JOURNEY["AUTH"], logger.MESSAGE["ERROR"]["TOKEN_VALIDITY"])
 		return ierrors.NewUnauthorizedError()
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["OK"]["TOKEN_VERIFY"])
 	return nil
 }
