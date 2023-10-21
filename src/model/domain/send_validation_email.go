c0f9b1100072b6a4eb73f966caa69e11462f84de
william-tome <william.tome@azion.com>
2023-10-21 18:49:37 -0300

    feat: implement logger in send email method

--- src/model/domain/send_validation_email.go
+++ src/model/domain/send_validation_email.go
@@ -6,9 +6,12 @@ import (
 
 	es "github.com/william-cesar/crud-in-go/src/config/email"
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 )
 
 func generateActivationMailTemplate(id string) string {
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE"], logger.MESSAGE["INIT"]["TEMPLATE"])
+
 	baseUrl, version := os.Getenv("BASE_URL"), os.Getenv("ENTRYPOINT")
 
 	activationLink := fmt.Sprintf("%s/%s/activate/%s", baseUrl, version, id)
@@ -19,16 +22,21 @@ func generateActivationMailTemplate(id string) string {
 		<a href="%s" target="_blank">Activate account</a>`, activationLink,
 	)
 
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE"], logger.MESSAGE["OK"]["TEMPLATE"])
 	return templateStr
 }
 
 func (u *tUser) SendActivationEmail(email, id string) *ierrors.TError {
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE"], logger.MESSAGE["INIT"]["SEND"])
+
 	subject := "Activate your account"
 	template := generateActivationMailTemplate(id)
 
 	if err := es.NewEmailService().SendActivationEmail(email, subject, template); err != nil {
+		logger.NewErrorLog(logger.JOURNEY["ACTIVATE"], logger.MESSAGE["ERROR"]["SEND_EMAIL"])
 		return err
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE"], logger.MESSAGE["OK"]["SENT"])
 	return nil
 }
