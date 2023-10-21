eb6f27d5a4d13af534d6d7f404adc5ce0633cdf3
william-tome <william.tome@azion.com>
2023-10-21 19:18:52 -0300

    feat: implement logger in login service

--- src/model/service/login_user_service.go
+++ src/model/service/login_user_service.go
@@ -2,15 +2,20 @@ package service
 
 import (
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"github.com/william-cesar/crud-in-go/src/model/domain"
 )
 
 func (us *tUserService) LoginUserService(user domain.IUser) (string, *ierrors.TError) {
+	logger.NewInfoLog(logger.JOURNEY["LOGIN_SERVICE"], logger.MESSAGE["INIT"]["LOGIN"])
+
 	user.EncryptPassword()
 
 	if err := us.findUserByEmailAndPassword(user); err != nil {
+		logger.NewErrorLog(logger.JOURNEY["LOGIN_SERVICE"], logger.MESSAGE["ERROR"]["NO_USER"])
 		return "", err
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["LOGIN_SERVICE"], logger.MESSAGE["OK"]["LOGGED"])
 	return user.GenerateToken()
 }
