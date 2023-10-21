da867f5c2b4ea45e8c06a3673fdc3b472c1c9073
william-tome <william.tome@azion.com>
2023-10-21 17:58:09 -0300

    feat: implement logger in db connection

--- src/config/database/mongodb/connection.go
+++ src/config/database/mongodb/connection.go
@@ -2,27 +2,31 @@ package mongodb
 
 import (
 	"context"
-	"log"
 	"os"
 
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func NewMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
+	logger.NewInfoLog(logger.JOURNEY["DB"], logger.MESSAGE["INIT"]["DB"])
+
 	conn, db := os.Getenv("DB_CONNECTION"), os.Getenv("DB_NAME")
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(conn))
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["DB"], logger.MESSAGE["ERROR"]["DB_CONN"])
 		return nil, err
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
+		logger.NewErrorLog(logger.JOURNEY["DB"], logger.MESSAGE["ERROR"]["DB_PING"])
 		return nil, err
 	}
 
-	log.Println("Connected to MongoDB")
+	logger.NewInfoLog(logger.JOURNEY["DB"], logger.MESSAGE["OK"]["DB_CONN"])
 
 	return client.Database(db), nil
 }
