f86c5bd463658be7b5dfc871525d0377e2104cd1
william-tome <william.tome@azion.com>
2023-10-21 18:41:55 -0300

    feat: implement logger in auth middleware

--- src/controller/middlewares/auth.go
+++ src/controller/middlewares/auth.go
@@ -3,13 +3,17 @@ package middlewares
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"github.com/william-cesar/crud-in-go/src/controller/handlers"
 )
 
-func Authorized(c *gin.Context) {
+func Auth(c *gin.Context) {
+	logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["INIT"]["AUTH"])
+
 	token := c.GetHeader("Authorization")
 
 	if token == "" {
+		logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["ERROR"]["NO_TOKEN"])
 		err := ierrors.NewUnauthorizedError()
 		c.AbortWithStatusJSON(err.StatusCode, err)
 	}
@@ -17,8 +21,10 @@ func Authorized(c *gin.Context) {
 	err := handlers.VerifyToken(token)
 
 	if err != nil {
+		logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["ERROR"]["INVALID_TOKEN"])
 		c.AbortWithStatusJSON(err.StatusCode, err)
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["AUTH"], logger.MESSAGE["OK"]["AUTH"])
 	c.Next()
 }
