97973c6ae018c2435cc454791abe2b1165a1ee37
william-tome <william.tome@azion.com>
2023-10-21 18:56:56 -0300

    feat: implement logger in activate user repository

--- src/model/repository/activate_user_repository.go
+++ src/model/repository/activate_user_repository.go
@@ -4,14 +4,22 @@ import (
 	"context"
 
 	"github.com/william-cesar/crud-in-go/src/config/ierrors"
+	"github.com/william-cesar/crud-in-go/src/config/logger"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (ur *tUserRepository) ActivateUser(id string) *ierrors.TError {
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE_REPOSITORY"], logger.MESSAGE["INIT"]["ACTIVATION"])
+
 	collection := ur.dbconn.Collection(COLLECTION)
 
-	userId, _ := primitive.ObjectIDFromHex(id)
+	userId, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["ACTIVATE_REPOSITORY"], logger.MESSAGE["ERROR"]["INVALID_ID"])
+		return ierrors.NewBadRequestError()
+	}
 
 	filter := bson.D{{Key: "_id", Value: userId}}
 	field := bson.D{{Key: "$set", Value: bson.D{{Key: "active", Value: true}}}}
@@ -20,15 +28,19 @@ func (ur *tUserRepository) ActivateUser(id string) *ierrors.TError {
 
 	if res.ModifiedCount == 0 {
 		if res.MatchedCount == 1 {
+			logger.NewWarningLog(logger.JOURNEY["ACTIVATE_REPOSITORY"], logger.MESSAGE["WARN"]["ACTIVE"])
 			return ierrors.NewBadRequestError("User already activated.")
 		}
 
+		logger.NewErrorLog(logger.JOURNEY["ACTIVATE_REPOSITORY"], logger.MESSAGE["ERROR"]["NO_USER"])
 		return ierrors.NewNotFoundError()
 	}
 
 	if err != nil {
+		logger.NewErrorLog(logger.JOURNEY["ACTIVATE_REPOSITORY"], logger.MESSAGE["ERROR"]["ACTIVATION"])
 		return ierrors.NewInternalError()
 	}
 
+	logger.NewInfoLog(logger.JOURNEY["ACTIVATE_REPOSITORY"], logger.MESSAGE["OK"]["ACTIVE"])
 	return nil
 }
