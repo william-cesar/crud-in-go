a0bca581e7e444200a799739adb8566e99ca830b
william-tome <william.tome@azion.com>
2023-10-21 17:57:00 -0300

    feat: create logger service

---
+++ src/config/logger/ilogger.go
@@ -0,0 +1,22 @@
+package logger
+
+type Options string
+type TprefixOption map[Options]string
+
+const (
+	INFO  Options = "INFO"
+	ERROR Options = "ERROR"
+	WARN  Options = "WARN"
+)
+
+var prefix = TprefixOption{
+	INFO:  "[INFO] - ",
+	ERROR: "[ERROR] - ",
+	WARN:  "[WARN] - ",
+}
+
+type TLogger struct {
+	Prefix  Options
+	Journey string
+	Message string
+}
---
+++ src/config/logger/logger.go
@@ -0,0 +1,58 @@
+package logger
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+)
+
+const logFile = "logs.log"
+
+func logCaller() string {
+	_, file, line, _ := runtime.Caller(3)
+	return fmt.Sprintf("%s:%d", file, line)
+}
+
+func newLog(logInfo *TLogger) {
+	logFile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+
+	log.SetPrefix(prefix[logInfo.Prefix])
+
+	info := fmt.Sprintf("%s - %s", logInfo.Journey, logInfo.Message)
+	log.Println(info)
+}
+
+func NewInfoLog(journey, message string) {
+	newLog(&TLogger{
+		Prefix:  INFO,
+		Journey: journey,
+		Message: message,
+	})
+}
+
+func NewErrorLog(journey, message string) {
+	message = fmt.Sprintf("%s - %s", message, logCaller())
+
+	newLog(&TLogger{
+		Prefix:  ERROR,
+		Journey: journey,
+		Message: message,
+	})
+}
+
+func NewWarningLog(journey, message string) {
+	newLog(&TLogger{
+		Prefix:  WARN,
+		Journey: journey,
+		Message: message,
+	})
+}
