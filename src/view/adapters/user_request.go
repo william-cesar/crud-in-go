a63c59f0799aac4fb0152750f89aa9a3a769725e
william-tome <william.tome@azion.com>
2023-10-21 22:54:35 -0300

    fix: add maxlen to email

--- src/view/adapters/user_request.go
+++ src/view/adapters/user_request.go
@@ -2,7 +2,7 @@ package adapters
 
 type TUserRequest struct {
 	Name     string `json:"name" binding:"required,min=2,max=100"`
-	Email    string `json:"email" binding:"required,email"`
+	Email    string `json:"email" binding:"required,email,max=100"`
 	Password string `json:"password" binding:"required,min=6,max=64,containsany=!@#$%&_-^~/?"`
 	Age      int8   `json:"age" binding:"required,min=1,max=150"`
 }
